cf4b82d83e4fb3e35108355ccd7aa3185b8eb2d6
Kattalunikes <kattalunikes+git@gmail.com>
2026-05-13 17:16:06 +0000

    Season 2 full

--- make-pdf.sh
+++ make-pdf.sh
@@ -4,7 +4,7 @@ pandoc -o moby-purpura.$TYPE --toc --toc-depth=2 \
   Season\ 01/*md \
   Season\ 02/*md \
   Season\ 03/*md \
-  --metadata title="Purpur Horizone" \
+  --metadata title="Purpura!" \
   --metadata subtitle="Die Legenden der Moby Purpura" \
   --metadata author="von Fear und der Crew aus Tentakelhausen" \
   --metadata author="(Amii, Devil, Luna, Luxi, Mitch, Purpur, Vio)" \
